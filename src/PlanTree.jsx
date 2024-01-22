2d94d9f96c73b49da3eadd18fbdcbd69524fb305
21MoududAhammed <21moudud@gmail.com>
2024-01-22 14:51:27 +0600

    now the delete button is working , if we click a delete button it will remove this element or list

--- src/PlanTree.jsx
+++ src/PlanTree.jsx
@@ -1,13 +1,17 @@
-export default function PlanTree({ travelPlan, id }) {
+export default function PlanTree({ travelPlan, onDelete, id, parentId }) {
   const place = travelPlan[id];
 
   return (
     <li>
-      {place.title}
+      {place.title} <button onClick={()=>onDelete(place.id, parentId)}>Delete</button>
       {place.childIds.length > 0 && (
         <ol>
           {place.childIds.map((id) => (
-            <PlanTree key={id} travelPlan={travelPlan} id={id} />
+            <PlanTree key={id} 
+            travelPlan={travelPlan}
+            onDelete={onDelete} 
+            id={id}
+            parentId={place.id} />
           ))}
         </ol>
       )}
--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -6,10 +6,28 @@ export default function Travel() {
   const root = travelPlan[0];
   const childIds = root.childIds;
 
+  function handleDelete(childId, parentId) {
+    const parent = travelPlan[parentId];
+    const nextParent = {
+        ...parent, 
+        childIds: parent.childIds.filter(id => id !== childId),
+    }
+    setTravelPlan({
+        ...travelPlan, 
+        [parentId]: nextParent,
+    })
+  }
+
   return (
     <ol>
       {childIds.map((id) => (
-        <PlanTree key={id} travelPlan={travelPlan} id={id} />
+        <PlanTree
+          key={id}
+          travelPlan={travelPlan}
+          onDelete={handleDelete}
+          id={id}
+          parentId={0}
+        />
       ))}
     </ol>
   );
